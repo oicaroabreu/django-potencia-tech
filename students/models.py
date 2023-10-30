3d4708e782d43f9e43d1f2ad7f7aaf0b2b8a27b5
Ícaro Abreu <136408366+oicaroabreu@users.noreply.github.com>
2023-10-30 10:10:04 -0300

    fix:min_max_int_fields

--- students/models.py
+++ students/models.py
@@ -1,20 +1,29 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Student(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="ID")
     nome = models.CharField(max_length=100, verbose_name="Nome")
-    idade = models.IntegerField(verbose_name="Idade")
+    idade = models.IntegerField(verbose_name="Idade", validators=[MinValueValidator(0), MaxValueValidator(200)])
     nota_do_primeiro_semestre = models.DecimalField(
-        max_digits=5, decimal_places=2, verbose_name="Nota do primeiro semestre"
+        max_digits=5,
+        decimal_places=2,
+        verbose_name="Nota do primeiro semestre",
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
     )
     nota_do_segundo_semestre = models.DecimalField(
-        max_digits=5, decimal_places=2, verbose_name="Nota do segundo semestre"
+        max_digits=5,
+        decimal_places=2,
+        verbose_name="Nota do segundo semestre",
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
     )
     nome_do_professor = models.CharField(
         max_length=100, verbose_name="Nome do professor"
     )
-    numero_da_sala = models.IntegerField(verbose_name="Número da sala")
+    numero_da_sala = models.IntegerField(
+        verbose_name="Número da sala", validators=[MinValueValidator(0), MaxValueValidator(10000)]
+    )
 
     def __str__(self):
         return self.name
